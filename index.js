be0d7748b5a192e90ba1bf497b5e0ea177cbc61b
next-jimmycollazos <jimmy.collazos.utrilla.next@bbva.com>
2020-02-11 11:41:39 +0100

    1 file modified

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 // Import stylesheets
 // import './style.css';
 
+import "@webcomponents/webcomponentsjs/webcomponents-bundle";
+import '@webcomponents/webcomponentsjs/custom-elements-es5-adapter.js';
 class AppDrawer extends HTMLElement {
 
   static get is() { return 'app-drawer' }
 
-  static get observedAttributes() {
-    return ['disabled', 'open'];
-  }
+  //static get observedAttributes() {
+    //return ['disabled', 'open'];
+  //}
   constructor() {
     super(); // always call super() first in the constructor.
     //this.innerHTML = "hello world!"
